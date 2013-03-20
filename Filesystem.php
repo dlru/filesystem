f203a9577bda5d96829d62a5c769b3c7a83b47b8
Victor Yasinovsky <yasinovsky@gmail.com>
2013-03-20 17:30:41 +0400

    Update headers

--- Filesystem.php
+++ Filesystem.php
@@ -2,8 +2,40 @@
 /**
  * DesignLab Framework
  *
+ * Copyright (c) 2001-2013, DesignLab, LLC. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  -   Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *  -   Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *
+ *  -   Neither the name of the DesignLab, LLC nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
  * @copyright   2001-2013 DesignLab, LLC.
  * @author      Victor Yasinovsky <victor@designlab.ru>
+ * @license     http://opensource.org/licenses/BSD-3-Clause
  * @link        http://www.designlab.ru
  */
 
@@ -90,7 +122,7 @@ abstract class Filesystem {
             $classname = __CLASS__ . '\\' . self::_get_driver_name();
             if (!class_exists($classname)) {
                 throw new FS_Exception( // По какой-то причине нет драйвера
-                    'Filesystem driver for "'.PHP_OS.'" is not exists'
+                    'Filesystem driver for "' . PHP_OS . '" is not exists'
                 );
             }
             // Создаем экземпляр драйвера
